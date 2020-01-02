d10e12aa4ed38127407bf183084eba8382b5ae9a
beaubouchard <beau@beaubouchard.com>
2020-01-02 12:02:00 -0500

    feat: starting to figure out the DB

---
+++ src/server.js
@@ -0,0 +1,55 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('server-side-tools').logger;
+const format = require('server-side-tools').format;
+const pkjson = require('../package.json');
+
+const app = express();
+
+let requestsCount = 0;
+
+// adding helmet to enhance api security
+app.use(helmet());
+
+// using bodyParser to parse json bodies into js objects
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+/** set up cors middleware
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {Next} next - Express Next object
+ */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, Origin, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  next();
+});
+
+logger.info('turning on app...');
+
+/**
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {Next} next - Express Next object
+ */
+app.get('/health', (req, res, next) => {
+  requestsCount++;
+  const time = process.uptime();
+  const uptime = format.toDDHHMMSS(time + '');
+  logger.info(`/health request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
+  res.status(200).send({ data: {uptime: uptime, version: pkjson.version, requests: requestsCount} });
+});
+
+// heroku dynamically assigns your app a port, so you can't set the port to a fixed number.
+var server = app.listen(process.env.PORT || 5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
+
+module.exports = server;
